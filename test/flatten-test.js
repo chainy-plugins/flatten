ca6fb16801f94d44969aa0d27c5080c412a543bd
Benjamin Lupton <b@lupton.cc>
2014-05-21 17:53:24 +1000

    v0.1.0. Abstracted out.
    - v0.1.0 May 21, 2014
    	- Abstracted out of chainy core

---
+++ test/flatten-test.js
@@ -0,0 +1,21 @@
+(function(){
+	// Import
+	var expect = require('chai').expect,
+		joe = require('joe')
+
+	// Test
+	joe.describe('flatten plugin', function(describe,it){
+		var Chainy = require('chainy-core').subclass().require('set').addExtension('flatten', require('../'))
+
+		it("should work", function(next){
+			Chainy.create()
+				.set([1, [2], [3, [[4]]]])
+				.flatten()
+				.done(function(err, result){
+					if (err)  return next(err)
+					expect(result).to.deep.equal([1, 2, 3, 4])
+					return next()
+				})
+		})
+	})
+})()
